f3732983395fcc1c5b6351fc5c94239860712603
Ander503 <89810321+Ander503@users.noreply.github.com>
2023-03-24 22:42:36 -0600

    Agregando Icono

--- Desafio_Practico_2/pages/Ingreso_D.php
+++ Desafio_Practico_2/pages/Ingreso_D.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-    <title>Document</title>
+    <link rel="shortcut icon" href="../img/LOGO1.png" type="image/x-icon">
+    <title>Ingreso Docente</title>
 
     <link rel="stylesheet" href="../css/style_form.css">
     <link rel="stylesheet" href="../css/style_back.css">
