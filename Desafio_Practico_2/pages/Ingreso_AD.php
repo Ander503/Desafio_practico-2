60f20e7265df2011ab349ad89998eaa3f09d8e2f
Ander503 <89810321+Ander503@users.noreply.github.com>
2023-03-24 22:41:41 -0600

    Agregando icono

--- Desafio_Practico_2/pages/Ingreso_AD.php
+++ Desafio_Practico_2/pages/Ingreso_AD.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-    <title>Document</title>
-
+    <title>Ingreso Administrativo</title>
+    <link rel="shortcut icon" href="../img/LOGO1.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/style_form.css">
     <link rel="stylesheet" href="../css/style_back.css">
     <!--Ingresar
